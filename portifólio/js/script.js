e6233bde9cd05c06f4d7062d5719423b4abd101c
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2023-05-27 17:26:06 -0300

    Finalizando Switch Dark/Light Mode

--- portifólio/js/script.js
+++ portifólio/js/script.js
@@ -1,5 +1,22 @@
-var btnToggle = document.querySelector(".switch")
+const html = document.querySelector('html')
+const btnToggle = document.querySelector(".switch")
+
+var git = document.querySelector(".img-git")
+var gitHover = document.querySelector(".img-git-hover")
+var gitPreto = document.querySelector(".img-git-preto")
+var gitHoverPreto = document.querySelector(".img-git-hover-preto")
+
+
+var link = document.querySelector(".img-link")
+var linkHover = document.querySelector(".img-link-hover")
+var insta = document.querySelector(".img-insta")
+var instaHover = document.querySelector(".img-insta-hover")
+
+btnToggle.addEventListener("change", ()=> {
+    html.classList.toggle('light')
+})
 
 btnToggle.addEventListener("click", ()=> {
-    alert('teste')
+    git.style
 })
+
