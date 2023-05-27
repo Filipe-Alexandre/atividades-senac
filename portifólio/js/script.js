620ea462b03bef339a1f4e117ac490e08656b9c6
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2023-05-27 17:29:28 -0300

    organizando variáveis

--- portifólio/js/script.js
+++ portifólio/js/script.js
@@ -1,17 +1,6 @@
 const html = document.querySelector('html')
 const btnToggle = document.querySelector(".switch")
 
-var git = document.querySelector(".img-git")
-var gitHover = document.querySelector(".img-git-hover")
-var gitPreto = document.querySelector(".img-git-preto")
-var gitHoverPreto = document.querySelector(".img-git-hover-preto")
-
-
-var link = document.querySelector(".img-link")
-var linkHover = document.querySelector(".img-link-hover")
-var insta = document.querySelector(".img-insta")
-var instaHover = document.querySelector(".img-insta-hover")
-
 btnToggle.addEventListener("change", ()=> {
     html.classList.toggle('light')
 })
