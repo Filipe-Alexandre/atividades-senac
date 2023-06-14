1527e137df481359d163a78c45e0ae5f8a185c51
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2023-06-14 19:15:37 -0300

    aaaaa
    troca de classe por id

--- agencia-cloud-wise/js/script.js
+++ agencia-cloud-wise/js/script.js
@@ -1,21 +1,36 @@
-// criar um identificador pra tela pequena pra ativar o slick add class cards__tab
-let cards = document.querySelector(".cards");
+// function adicionarOuRemoverId() {
+//   var larguraTela = window.innerWidth || document.documentElement.clientWidth;
 
-function checarTela() {
-  let largura = window.innerWidth;
-  if (largura <= 769) {
-    document.documentElement.classList.add("cards__tab");
-  } else {
-    document.documentElement.classList.remove("cards__tab");
-  }
-}
+//   // Seleciona o elemento pelo seletor
+//   var elemento = document.querySelector(".cards");
 
-window.addEventListener("load", checarTela);
-window.addEventListener("resize", checarTela);
+//   if (larguraTela <= 769) {
+//     // Adiciona o ID ao elemento
+//     if (!elemento.id) {
+//       elemento.id = "cards__tab";
+//     }
+//   } else {
+//     // Remove o ID do elemento
+//     if (elemento.id === "cards__tab") {
+//       elemento.removeAttribute("id");
+//     }
+//   }
+// }
+
+// // Adiciona um ouvinte de evento 'resize' à janela
+// window.addEventListener('resize', function() {
+//   // Verifica se a largura da tela corresponde à condição desejada
+//   if (window.matchMedia("(max-width: 769px)").matches) {
+//     adicionarOuRemoverId();
+//   }
+// });
+
+// // Executa a função inicialmente para verificar a largura da tela quando a página é carregada
+// adicionarOuRemoverId();
 
 // -----------------------------------------------------------------
 
-$(".cards__tab").slick({
+$("#cards__tab").slick({
   infinite: true,
   slidesToShow: 2,
   slidesToScroll: 1,
