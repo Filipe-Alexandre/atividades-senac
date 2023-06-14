235231c0c97e1e71d9c7ba5d74dba4c0032626a4
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2023-06-14 17:27:55 -0300

    teste js

--- agencia-cloud-wise/js/script.js
+++ agencia-cloud-wise/js/script.js
@@ -1,8 +1,19 @@
-// criar um identificador pra tela pequena pra ativar o slick add class cards-769
+// criar um identificador pra tela pequena pra ativar o slick add class cards__tab
+let cards = document.querySelector(".cards");
 
+function checarTela() {
+  let largura = window.innerWidth;
+  if (largura <= 769) {
+    document.documentElement.classList.add("cards__tab");
+  } else {
+    document.documentElement.classList.remove("cards__tab");
+  }
+}
+
+window.addEventListener("load", checarTela);
+window.addEventListener("resize", checarTela);
 
 // -----------------------------------------------------------------
-
 
 $(".cards__tab").slick({
   infinite: true,
