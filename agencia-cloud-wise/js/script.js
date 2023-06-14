9478cb6d7aed0d26fa6217d38d8e123930d3901c
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2023-06-14 16:29:23 -0300

    configurando carrossel

---
+++ agencia-cloud-wise/js/script.js
@@ -0,0 +1,14 @@
+// criar um identificador pra tela pequena pra ativar o slick add class cards-769
+
+
+// -----------------------------------------------------------------
+
+
+$(".cards__tab").slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  infinite: false,
+  speed: 300,
+});
