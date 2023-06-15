0ca6ad23b7e40a4a8a4246ad3ce00cff0dd5bed9
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2023-06-15 17:23:25 -0300

    organizando svc tab

--- agencia-cloud-wise/js/script.js
+++ agencia-cloud-wise/js/script.js
@@ -1,35 +1,28 @@
+// tentar fazer função de reconhecer a tela DE NOVO
+// let cards = document.querySelectorAll('.cards');
+// let largura = window.innerWidth
 
-function adicionarOuRemoverId() {
-  var larguraTela = window.innerWidth || document.documentElement.clientWidth;
+// function teste () {
+//   if(largura <= 769) {
+//     document.documentElement.classList.add('cards cards__tab');
+//   } else {
+//     document.documentElement.classList.remove('cards cards__tab');
+//   }
 
-  // Seleciona o contêiner pelo seletor
-  var container = document.querySelector(".cards");
+//   // verificação
 
-  if (larguraTela === 769) {
-    // Adiciona o ID ao contêiner
-    if (!container.id) {
-      container.id = "cards__tab";
-    }
-  } else {
-    // Remove o ID do contêiner
-    if (container.id === "cards__tab") {
-      container.removeAttribute("id");
-    }
-  }
-}
+//   window.addEventListener('load', teste());
+//   window.addEventListener('resize', teste());
 
-// Adiciona um ouvinte de evento 'resize' à janela
-window.addEventListener('resize', adicionarOuRemoverId);
+//   teste();
+// }
 
-// Executa a função inicialmente para verificar a largura da tela quando a página é carregada
-adicionarOuRemoverId();
-// -----------------------------------------------------------------
+//-----------------------------------------------------------------
 
-$("#cards__tab").slick({
+$(".cards__tab").slick({
   infinite: true,
   slidesToShow: 2,
   slidesToScroll: 1,
   autoplay: true,
-  infinite: false,
-  speed: 300,
+  speed: 00,
 });
