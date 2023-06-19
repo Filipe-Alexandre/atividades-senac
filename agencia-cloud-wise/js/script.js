58cd0bedf6a98ec04e9a39ce7ae5167f57aa90e8
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2023-06-19 17:26:43 -0300

    menu burger

--- agencia-cloud-wise/js/script.js
+++ agencia-cloud-wise/js/script.js
@@ -1,10 +1,12 @@
-// tentar fazer função de reconhecer a tela DE NOVO
-// let cards = document.querySelectorAll('.cards');
+// // tentar fazer função de reconhecer a tela DE NOVO
+
+// let cards = document.querySelectorAll('.texto1');
 // let largura = window.innerWidth
 
 // function teste () {
-//   if(largura <= 769) {
-//     document.documentElement.classList.add('cards cards__tab');
+//   if(largura <= 600) {
+//     cards.innerHtml = '<p> gaiufdjaifnsojdnf </p>'
+
 //   } else {
 //     document.documentElement.classList.remove('cards cards__tab');
 //   }
@@ -37,9 +39,26 @@ $(".blogs").slick({
     {
       breakpoint: 769,
       settings: {
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1
       }
     },
   ]
 });
+
+//-----------------------------------------------------------------
+
+let open = document.querySelector(".btn-open")
+let close = document.querySelector(".btn-close")
+let nav = document.querySelector('.nav')
+
+// buscar o elemento do nav e dar um display block
+
+
+open.addEventListener('click', () => {
+  document.nav.style.display = 'block'
+})
+
+close.addEventListener('click', () => {
+  document.documentElement.classList.remove("menu-ativo")
+})
